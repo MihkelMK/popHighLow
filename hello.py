64a7184528dc6f73d4ce4695cb3e34d816dc32ec
Mihkel Martin Kasterpalu <mihkel@singleton.ee>
2021-11-23 21:22:29 +0200

    Wrote game logic, game playable with dummy info. Added communication between js and python. Using said communication to update visible info. Made win/lose/landing/game pages and score system.

--- hello.py
+++ hello.py
@@ -1,9 +1,73 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from random import choice
 
-@app.route("/")
+rawData = {
+         "Jürgen Tamm": [143, '../static/friendata/img/1.jpg'],
+         "Grethe Märts": [167, '../static/friendata/img/2.jpg'],
+         "Hugo Vaher": [10, '../static/friendata/img/3.jpg'],
+         "Päris Inimene": [486, '../static/friendata/img/4.jpg']}
+""" 
+rawData = {
+         "1": [1, '../static/friendata/img/1.jpg'],
+         "2": [2, '../static/friendata/img/2.jpg'],
+         "3": [3, '../static/friendata/img/3.jpg'],
+         "4": [4, '../static/friendata/img/4.jpg'],
+         "5": [5, '../static/friendata/img/1.jpg'],
+         "6": [6, '../static/friendata/img/2.jpg'],
+         "7": [7, '../static/friendata/img/3.jpg'],
+         "8": [8, '../static/friendata/img/4.jpg'],
+         "9": [9, '../static/friendata/img/1.jpg'],
+         "10": [10, '../static/friendata/img/2.jpg'],
+         "11": [11, '../static/friendata/img/3.jpg'],
+         "12": [12, '../static/friendata/img/4.jpg']} """
+
+kasutajaNimi = 'Jürgen Tamm'
+maxScore = len(rawData.keys())-1
+
+app = Flask(__name__)
+app.secret_key = b'41a2fc1a57d9edde2e44a19616490a330726f984ed7470953da97845c36224fb'
+app.config['SESSION_COOKIE_SAMESITE'] = "Strict"
+
+def randomSelect(viimaneNimi):
+   while True:
+      nimi = choice(list(session['people'].keys()))
+      if nimi != kasutajaNimi and nimi != viimaneNimi:
+         return nimi
+
+@app.route("/", methods=["GET"])
 def index():
    return render_template("index.html")
+
+@app.route("/game", methods=["GET"])
+def game():
+   session['people'] = rawData
+   session['score'] = 0
+   return render_template("game.html")
+
+@app.route("/win", methods=["GET"])
+def win():
+   return render_template("win.html", score=session.get('score', None))
+
+@app.route("/lose", methods=["GET"])
+def lose():
+   return render_template("lose.html", score=session.get('score', None), maxScore=maxScore)
+
+@app.route("/game", methods=["POST"])
+def endpoint():
+   nimi = str(request.get_json('nimi')['nimi'])
+
+   if nimi == 'kasutaja':
+      randomFriend = randomSelect(nimi)
+      return jsonify({'given': [kasutajaNimi, session['people'].pop(kasutajaNimi)], 'guess': [randomFriend, session['people'][randomFriend]], 'score': session['score']})
+   else:
+      session['score'] += 1
+      if session['score'] == maxScore:
+         return jsonify({'võit': 'jah', 'score': session['score']})
+
+      randomFriend = randomSelect(nimi)
+      return jsonify({'given': [nimi, session['people'].pop(nimi)], 'guess': [randomFriend, session['people'][randomFriend]], 'score': session['score']})
+      
+
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/ohboy.js
@@ -0,0 +1,49 @@
+function request(name) {
+    let data = {'nimi': name}
+
+    return fetch("/game", {
+        method: "POST", 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }).then((response) => response.json())
+};
+
+const updatePeople = (nimi) => {
+    request(nimi).then((a) => {
+        document.getElementsByClassName('skoor')[0].textContent = 'Skoor: ' + a['score'];
+        if ('võit' in a) {
+            document.location.href = '/win';
+        }
+
+        let givenName = a['given'][0];
+        let guessName = a['guess'][0];
+        let givenNr = a['given'][1][0];
+        let guessNr = a['guess'][1][0];
+        
+        
+        document.getElementById('givenNr').textContent = givenNr;
+        document.getElementById('guessNr').textContent = guessNr;
+        document.getElementById('givenName').textContent = givenName;
+        document.getElementById('guessName').textContent = guessName;
+        document.getElementsByClassName('given')[0].style.cssText += `background-image: linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${a['given'][1][1]});`;
+        document.getElementsByClassName('guess')[0].style.cssText += `background-image: linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${a['guess'][1][1]});`;
+    });
+};
+
+const checkGuess = (guess) => {
+    let givenNr = Number(document.getElementById('givenNr').textContent);
+    let guessNr = Number(document.getElementById('guessNr').textContent);
+    let guessName = document.getElementById('guessName').textContent;
+
+    if (guess == 'rohkem' && givenNr <= guessNr) {
+        updatePeople(guessName)
+    }
+    else if (guess == 'vähem' && givenNr >= guessNr) {
+        updatePeople(guessName)
+    }
+    else {
+        document.location.href = '/lose';
+    }
+}
+
+updatePeople('kasutaja');
